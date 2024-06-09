1dcc09fed045cbf742c8499b327fed047ec105b8
Ritik Choudhary <ritikmon@gmail.com>
2024-06-10 01:56:28 +0530

    updated portfolio

--- js/script.js
+++ js/script.js
@@ -13,7 +13,7 @@ let navLinks = document.querySelectorAll('header nav a');
 window.onscroll = () =>{
     sections.forEach(sec =>{
         let top=window.scrollY;
-        let offset=sec.offsetTop-400;
+        let offset=sec.offsetTop-250;
         let height = sec.offsetHeight;
         let id=sec.getAttribute('id');
 
